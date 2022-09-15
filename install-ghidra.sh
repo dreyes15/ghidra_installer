7516775f85960bcd14fecef9ff55bb6d4e997175
David Reyes <dreyes15@miners.utep.edu>
2022-09-14 23:30:21 -0600

    Finished up install script with the option for a custom ghidra install, final testing needed

--- install-ghidra.sh
+++ install-ghidra.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 INSTALL_DIR=/usr/share
 
-echo "Downloading Ghidra and installing to $INSTALL_DIR"
 export WGET=`which wget`
 export SUDO=`which sudo 2> /dev/null`
 test -e ./install-ghidra.sh || { echo Error: you must run the script from the ./install_ghidra/ directory ; exit 1 ; }
@@ -9,6 +8,7 @@ test -z "$WGET" && { echo Error: wget not found ; exit 1 ; }
 
 
 function install_java {
+
   echo "Checking to see if Java is installed..."
   PKG_OK=$(dpkg-query -W --showformat='${Status}\n' openjdk-11-jdk | grep "install ok installed")
 
@@ -20,7 +20,9 @@ function install_java {
   fi
 }
 
+#Turning off 4K scaling
 function 4k_scaling {
+  
   read -p "Shall I change scaling to factor 2 for 4K [Y/N]? " -n 1 -r
   echo    # (optional) move to a new line
   if [[ $REPLY =~ ^[Yy]$ ]]
@@ -29,7 +31,9 @@ function 4k_scaling {
   fi
 }
 
+#Allow for .desktop file to execute without having to right click and slect "Alow Launching"
 function allow_desktop_launching {
+
   cp ghidra.desktop $HOME/.local/share/applications/ghidra.desktop
   #Allow for launching without having to right click on the desktop file and select "Allow Launching"
   gio set ~/Desktop/ghidra.desktop metadata::trusted true
@@ -37,6 +41,7 @@ function allow_desktop_launching {
 }
 
 function download_ghidra {
+
   #Link where to find Ghidra
   export GHIDRA_LINK=`$WGET -O - --quiet  https://github.com/NationalSecurityAgency/ghidra/releases/latest | grep 'releases/download/' | sed 's/.*href=..//' | sed 's/".*//' | tail -1`
   test -z "$GHIDRA_LINK" && { echo Error: could not find ghidra to download ; exit 1 ; }
@@ -72,8 +77,7 @@ function download_ghidra {
 }
 
 function unzip_ghidra {
-  echo
-  echo Unpacking Ghidra ...
+
   unzip "$GHIDRA" > /dev/null || exit 1
   mv "$GHIDRA_DIR" "$GHIDRA_VER"
   
@@ -88,6 +92,66 @@ function unzip_ghidra {
 
 }
 
+function copy_desktop_file {
+
+  for dir in Desktop ; do
+    echo "$dir"
+    test -d $HOME/$dir && {
+      cp ghidra.desktop $HOME/$dir/ghidra.desktop
+      chown $USER:$USER $HOME/$dir/ghidra.desktop
+    }
+  done
+}
+
+function final_setup {
+
+  $SUDO rm -f /usr/bin/ghidra /usr/local/bin/ghidra 
+  $SUDO ln -s $INSTALL_DIR/ghidra/ghidraRun /usr/local/bin/ghidra
+
+  cd $INSTALL_DIR || exit 1
+  OLD_DIR=`readlink ghidra`
+  $SUDO ln -sf $GHIDRA_VER ghidra
+
+  test -n "$OLD_DIR" && {
+    echo "Syncing from previous ghidra direcory: $OLD_DIR"
+    RSYNC=`command -v rsync 2> /dev/null`
+    test -n "$RSYNC" && {
+      echo "Running $RSYNC to synchronize custom scripts to the new installation"
+      rsync -v -r --ignore-existing --exclude='*/jython*' "$DIR/Ghidra/" "$GHIDRA_VER/Ghidra/"
+    }
+    test -z "$RSYNC" && {
+      echo "Warning: rsync not found, using old and incomplete copy process ..."
+      echo "Copying customized scripts from $DIR to $GHIDRA_VER"
+      for dir in $OLD_DIR/Ghidra/*/*/ghidra_scripts/; do
+        cp -nrv "$DIR/$dir"/* "$GHIDRA_VER/$dir/" 2> /dev/null
+      done
+    }
+  }
+
+  GHIDRACFG=`echo .$GHIDRA_VER | tr _ -`
+  cd $HOME/.ghidra && {
+    DIR=
+    rm -rf $GHIDRACFG
+    ls -td .ghidra-* | while read dir; do
+      test '!' -L "$dir" -a -d "$dir" -a -z "$DIR" && {
+        DIR=$dir
+        ln -s $dir $GHIDRACFG
+        echo "Symlinking $HOME/.ghidra/$dir to $HOME/.ghidra/$GHIDRACFG"
+      }
+    done
+  }
+
+}
+
+function setup_ghidra_folder {
+
+  cp $Custom_Zip .
+  export GHIDRA=`echo $Custom_Zip | sed 's/^.*\(ghidra.*\).*$/\1/' `
+  export GHIDRA_DIR=`echo $GHIDRA | sed 's/_20[12][0-9].*//' `
+  export GHIDRA_VER=`echo $GHIDRA | sed 's/_PUBLIC_.*//' | sed 's/_DEV_.*//' | sed 's/-BETA_.*//'`
+  
+}
+
 #Checking to see if Java JDK is installed if not Install it first
 install_java
 
@@ -98,76 +162,35 @@ while true; do
 
   case $yn in 
   	[yY] )
-      read -p "Input the filepath to your custom Ghidra zipfile " custom_zip
-      echo "$custom_zip"
-
+      read -p "Input the filepath to your custom Ghidra zipfile " Custom_Zip
+      echo "Installing custom Ghidra to $INSTALL_DIR ..."
+      echo "Unpacking Ghidra ..."
+      setup_ghidra_folder
+      unzip_ghidra
+      echo "Copying .desktop file and allowing for execution ..."
+      copy_desktop_file
+      allow_desktop_launching
+      final_setup
+      4k_scaling
+      echo "Successfully installed custom Ghidra version $GHIDRA_VER to $INSTALL_DIR/$GHIDRA_DIR"
+      echo "Run using: ghidra"
   		break;;
 
   	[nN] ) 
-      echo
-      echo "Downloading Official release from Github";
+      echo "Downloading Ghidra and installing to $INSTALL_DIR"
+      echo "Downloading Official release from Github ...";
       download_ghidra
+      echo "Unpacking Ghidra ..."
       unzip_ghidra
-
+      echo "Copying .desktop file and allowing for execution ..."
+      copy_desktop_file
+      allow_desktop_launching
+      final_setup
+      4k_scaling
+      echo "Successfully installed Ghidra version $GHIDRA_VER to $INSTALL_DIR/$GHIDRA_DIR"
+      echo "Run using: ghidra"
       exit;;
 
   	* ) echo invalid response;;
   esac
-
-  done
-
-
-for dir in Desktop Schreibtisch; do
-  test -d $HOME/$dir && {
-    cp ghidra.desktop $HOME/$dir/ghidra.desktop
-    chown $USER:$USER $HOME/$dir/ghidra.desktop
-  }
 done
-#
-##Copy the ghidra.desktop file to the the desktop and allow for execution
-##Without having to right click and slect "Alow Launching"
-#allow_desktop_launching
-#
-#$SUDO rm -f /usr/bin/ghidra /usr/local/bin/ghidra 
-#$SUDO ln -s $INSTALL_DIR/ghidra/ghidraRun /usr/local/bin/ghidra
-#
-#cd $INSTALL_DIR || exit 1
-#OLD_DIR=`readlink ghidra`
-#$SUDO ln -sf $GHIDRA_VER ghidra
-#
-#test -n "$OLD_DIR" && {
-#  echo "Syncing from previous ghidra direcory: $OLD_DIR"
-#  RSYNC=`command -v rsync 2> /dev/null`
-#  test -n "$RSYNC" && {
-#    echo "Running $RSYNC to synchronize custom scripts to the new installation"
-#    rsync -v -r --ignore-existing --exclude='*/jython*' "$DIR/Ghidra/" "$GHIDRA_VER/Ghidra/"
-#  }
-#  test -z "$RSYNC" && {
-#    echo "Warning: rsync not found, using old and incomplete copy process ..."
-#    echo "Copying customized scripts from $DIR to $GHIDRA_VER"
-#    for dir in $OLD_DIR/Ghidra/*/*/ghidra_scripts/; do
-#      cp -nrv "$DIR/$dir"/* "$GHIDRA_VER/$dir/" 2> /dev/null
-#    done
-#  }
-#}
-#
-#
-#GHIDRACFG=`echo .$GHIDRA_VER | tr _ -`
-#cd $HOME/.ghidra && {
-#  DIR=
-#  rm -rf $GHIDRACFG
-#  ls -td .ghidra-* | while read dir; do
-#    test '!' -L "$dir" -a -d "$dir" -a -z "$DIR" && {
-#      DIR=$dir
-#      ln -s $dir $GHIDRACFG
-#      echo "Symlinking $HOME/.ghidra/$dir to $HOME/.ghidra/$GHIDRACFG"
-#    }
-#  done
-#}
-#
-##Turning off 4K scaling
-#4k_scaling
-#
-#echo
-#echo "Successfully installed Ghidra version $GHIDRA_VER to $INSTALL_DIR/$GHIDRA_DIR"
-#echo "Run using: ghidra"
